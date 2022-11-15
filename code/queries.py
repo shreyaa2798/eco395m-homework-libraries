cdc441404fde6b81e5249302090d1ee574502216
shreyaa2798 <112149651+shreyaa2798@users.noreply.github.com>
2022-11-14 23:21:28 -0600

    Initial commit

---
+++ code/queries.py
@@ -0,0 +1,39 @@
+# PROBLEM 1
+# How many rows are in the 2009 dataset?
+# Return a single column called "count" with a single row containing the count.
+query_1 = """
+    """
+
+# PROBLEM 2
+# How many rows are in the 2014 dataset?
+# Return a single column called "count" with a single row containing the count.
+query_2 = """
+"""
+
+# PROBLEM 3
+# What are the maximum and minimum values for visits for the 2014 dataset?
+# Return a two columns called "max" and "man" with a single row containing the respective values
+
+query_3 = """
+"""
+
+# PROBLEM 4
+# Find the total number of visits in each state for the 2014 dataset. Be careful about invalid values.
+# Return a two columns called "stabr" and "visits" with rows in descending order of visits.
+
+query_4 = """
+"""
+
+# PROBLEM 5
+# Find the difference in total visits from 2009 to 2014.
+# Return a single column called "change_in_visits" with a single row containing the total.
+
+query_5 = """
+"""
+
+# PROBLEM 6
+# Find the difference in total visits from 2009 to 2014 for libraries (fscskey) in both dataset.
+# Return a single column called "change_in_visits" with a single row containing the total.
+
+query_6 = """
+"""
